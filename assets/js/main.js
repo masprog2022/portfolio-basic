36ab1036a546c946f68e6785f8070685be228f0a
Mauro Manuel <MAURO.MANUEL@UNITEL.CO.AO>
2022-08-06 16:10:23 +0100

    update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,112 @@
-/*===== SHOW MENU =====*/
-/* MENU SHOW */ 
+const showMenu = (toggleId,navId) =>{
 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu')
-        })
-    }
+  if(toggle && nav){
+      toggle.addEventListener('click', () =>{
+          nav.classList.toggle('show-menu');
+      })
+  }
 }
-
-showMenu('nav-toggle','nav-menu')
-
-
-/*===== REMOVE MENU MOBILE =====*/
-
-const navLink = document.querySelectorAll('.nav__link')
+showMenu('nav_toggle','nav-menu');
+// REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction(){
-  const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
 }
+navLink.forEach(n => n.addEventListener('click',linkAction));
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+// Scroll section active link
 
-
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
-/*
-let mainNavLinks = document.querySelectorAll("nav ul li a");
-let mainSections = document.querySelectorAll("main section");
-
-let lastId;
-let cur = [];
-
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
-
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
-
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("active-link");
-    } else {
-      link.classList.remove("active-link");
-    }
-  });
-});
-
-*/
-
-
-const sections = document.querySelectorAll("section[id]")
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive(){
-    const scrollY = window.pageYOffset
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
+  sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 50;
+      sectionId = current.getAttribute('id');
 
-         const sectionId = current.getAttribute('id')
-
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+          document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+      }else{
+          document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+      }
+  })
 }
+window.addEventListener('scroll',scrollActive);
 
-window.addEventListener('scroll', scrollActive)
-
-
-/*===== CHANGE BACKGROUND HEADER =====*/ 
+// Change Background Header
 
 function scrollHeader(){
-    const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  const header =  document.getElementById('header');
+  if(this.scrollY >= 200) header.classList.add('scroll-header');
+      else header.classList.remove('scroll-header');
 }
-
-window.addEventListener('scroll', scrollHeader)
-
-
-/*===== SHOW SCROLL TOP =====*/ 
+window.addEventListener('scroll',scrollHeader);
 
 function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top')
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+  const scrollTop = document.getElementById('scroll-top');
+  if(this.scrollY >= 500) scrollTop.classList.add('show-scroll');
+      else scrollTop.classList.remove('show-scroll');
 }
+window.addEventListener('scroll',scrollTop);
 
-window.addEventListener('scroll', scrollTop)
-
-
-/*===== MIXITUP FILTER PORTFOLIO =====*/ 
-const mixer = mixitup('.portfolio__container', {
-    selectors: {
-        target: '.portfolio__content'
-    },
-    animation: {
-        duration: 400
-    }
+// MIXITUP Filter
+const mixer = mixitup('.portfolio_container', {
+  selectors: {
+      target: '.portfolio_content'
+  },
+  animation: {
+      duration: 400
+  }
 });
 
-/* Link active portfolio */ 
+// Link active portfolio
 
-const linkPortfolio = document.querySelectorAll('.portfolio__item')
+const linkPortfolio = document.querySelectorAll('.portfolio_item');
 
 function activePortfolio(){
   if(linkPortfolio){
-    linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
-    this.classList.add('active-portfolio')
+      linkPortfolio.forEach(l => l.classList.remove('active-portfolio'));
+      this.classList.add('active-portfolio');
   }
 }
-linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
+linkPortfolio.forEach(l => l.addEventListener('click',activePortfolio));
 
+// SWIPER
 
+const swiper = new Swiper('.testimonial_container', {
+  // Optional parameters
+  spaceBetween:16,
+  loop: true,
+  grabCursor: true,
+  // If we need pagination
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+  },
+  breakpoints:{
+      640:{
+          slidesPerView: 2,
+      },
+      1024:{
+          slidesPerView: 3,
+      },
+  }
+});  
 
-/*===== GSAP ANIMATION =====*/ 
-
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x:60})
-gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y:25})
-gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay:1, y:25, ease:'expo.out', stagger: .2})
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay:1.5, y:25, ease:'expo.out', stagger: .2})
-gsap.from('.nav__item, .nav__toggle', {opacity: 0, duration: 2, delay:1.8, y:25, ease:'expo.out', stagger: .2})
-gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay:2.3, y:25, ease:'expo.out', stagger: .2})
-
-
+// GSAP
+// gsap.form(".home_img",{opacity: 0, duration: 2, delay: .5, x: 60});
+// gsap.from(".home_img", {duration: 3, x: 300, opacity: 0, scale: 0.5});
+gsap.from(".home_img", {duration: 2, x: 60, opacity: 0, delay: .5});
+gsap.from(".home_data", {duration: 2, y: 25, opacity: 0, delay: .8});
+gsap.from(".home_greeting, .home_name, .home_profession,.home_button", {duration: 2, y: 25, opacity: 0, delay: 1, ease:'expo.out', stagger:.2});
+gsap.from(".nav_logo, .nav_toggle",{duration: 2, y: 25, opacity: 0, delay: 1.5, ease:'expo.out', stagger:.2});
+gsap.from(".nav_item",{duration: 2, y: 25, opacity: 0, delay: 1.8, ease:'expo.out', stagger:.2});
+gsap.from(".home_social-icon",{duration: 2, y: 25, opacity: 0, delay: 2.3, ease:'expo.out', stagger:.2});
